8be4afa58447b48d25fd6328cc9883f1feadeb6c
Laura Martin Davis <lauraomartin@icloud.com>
2026-01-18 10:50:53 -0500

    Initial Commit

---
+++ Football players/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football players
+//
+//  Created by Laura Becca Davis on 18/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football players/Football_playersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_playersApp.swift
+//  Football players
+//
+//  Created by Laura Becca Davis on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Football_playersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
